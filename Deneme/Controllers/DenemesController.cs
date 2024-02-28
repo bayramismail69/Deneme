b833796d33eaec073844310759b9a9dc415b7efc
bayramismail <bayramismail69@gmail.com>
2024-02-28 23:09:46 +0300

    updated

---
+++ Deneme/Controllers/DenemesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deneme.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DenemesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Gel(string path)
+        {
+            return Ok("Deneme: "+path);
+        }
+    }
+}
